8c0ca2c08a786be04b8fbc36c2f4975bfa649533
kuldeep3 <kuldeepupreti3@gmail.com>
2020-07-21 12:59:24 +0530

    recent changes

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,38 +1,8 @@
 import React from "react";
 import { View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: "white",
-        flex: 1,
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          width: 100,
-          height: 300,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "gold",
-          width: 100,
-          height: 200,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "tomato",
-          width: 100,
-          height: 100,
-        }}
-      />
-    </View>
-  );
+  return <ViewImageScreen />;
 }
