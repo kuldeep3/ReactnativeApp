b2fff7867b14c54fa1b4c30556d96696705b9d41
kuldeep3 <kuldeepupreti3@gmail.com>
2020-07-21 17:37:43 +0530

    recent changes

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -6,7 +6,8 @@ import AppButton from "./app/components/AppButton";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return <ListingDetailsScreen />;
+  return <ViewImageScreen />;
 }
