d5bcee8b43175e06dcc6721279124c6c97ac4a74
kuldeep3 <kuldeepupreti3@gmail.com>
2020-07-21 13:03:41 +0530

    recent changes

--- DoneWithIt/app/screens/ViewImageScreen.js
+++ DoneWithIt/app/screens/ViewImageScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Image, StyleSheet, View } from "react-native";
+import colors from "../config/colors";
 
 function ViewImageScreen(props) {
   return (
@@ -21,13 +22,13 @@ const styles = StyleSheet.create({
     height: "100%",
   },
   container: {
-    backgroundColor: "#000",
+    backgroundColor: colors.black,
     flex: 1,
   },
   closeIcon: {
     width: 50,
     height: 50,
-    backgroundColor: "#fc5c65",
+    backgroundColor: colors.primary,
     position: "absolute",
     top: 40,
     left: 30,
@@ -35,7 +36,7 @@ const styles = StyleSheet.create({
   deleteIcon: {
     width: 50,
     height: 50,
-    backgroundColor: "#fecdc4",
+    backgroundColor: colors.secondary,
     position: "absolute",
     top: 40,
     right: 30,
