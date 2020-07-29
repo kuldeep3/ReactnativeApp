114d93c2afb7934065f3e0a57fde135e6f28f188
kuldeep3 <kuldeepupreti3@gmail.com>
2020-07-29 16:35:56 +0530

    recent changes

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,6 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Screen from "./app/components/Screen";
-import { Switch } from "react-native";
+
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
+
+import { Switch, Button, Image } from "react-native";
 import AppTextInput from "./app/components/TextInput";
 import AppPicker from "./app/components/Picker";
 import LoginScreen from "./app/screens/LoginScreen";
@@ -9,12 +13,30 @@ import ListingScreen from "./app/screens/ListingScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App() {
+  // const requestPermission = async () => {
+  //   const { granted } = await ImagePicker.requestCameraPermissionsAsync();
+  //   if (!granted) alert("You need to enable permission to access the library.");
+  // };
+  // useEffect(() => {
+  //   requestPermission();
+  // }, []);
   // const categories = [
   //   { label: "Furniture", value: 1 },
   //   { label: "Food", value: 2 },
   //   { label: "Clothing", value: 3 },
   // ];
   // const [category, setCategory] = useState(categories[0]);
+
+  const [imageUri, setImageUri] = useState();
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled) setImageUri(result.uri);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     // <Screen>
     //   <AppPicker
@@ -26,6 +48,11 @@ export default function App() {
     //   />
     // </Screen>
     // <LoginScreen />
-    <ListingEditScreen />
+    // <ListingEditScreen />
+
+    <Screen>
+      <Button title="Select Image" onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+    </Screen>
   );
 }
